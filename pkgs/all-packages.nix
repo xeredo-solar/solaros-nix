11ffb5fa186400eba63df374e05e005fafa4f7f1
Maciej Krüger <mkg20001@gmail.com>
2020-08-05 08:03:55 +0200

    feat: use scope in overlay

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -1,8 +1,5 @@
 { lib, pkgs, ... }:
 let
-  nixNodePackage = pkgs.callPackage ./nix-node-package.nix {};
-  mkNode = import "${nixNodePackage}/nix/default.nix" pkgs;
-
   installerSrcData = builtins.fromJSON (builtins.readFile ./nixinstall/source.json);
   installerSrc = pkgs.fetchFromGitHub {
     repo = "nixinstall";
@@ -10,38 +7,49 @@ let
     rev = installerSrcData.rev;
     sha256 = installerSrcData.sha256;
   };
-  nixinstall = lib.recurseIntoAttrs (pkgs.callPackage "${installerSrc}/nix" { });
 
-  makeIcon = pkgs.callPackage ./make-icon {};
-
-  recursiveIterateRecreate = set: iter:
+  /* recursiveIterateRecreate = set: iter:
     builtins.listToAttrs(
       builtins.concatMap iter (builtins.attrNames set)
-    );
-
-  solaros-skel-base = pkgs.callPackage ./solaros-skel-base {};
-
+    ); */
 in
-{
+lib.makeScope pkgs.newScope (self: with self; {
+  # src
   solarosNixpkgs = import ../lib/nixpkgs.nix;
   solarosNixosHardware = import ../lib/nixos-hardware.nix;
   solaros = lib.cleanSource ../.;
+  nixNodePackage = callPackage ./nix-node-package.nix { };
 
-  inherit makeIcon nixNodePackage nixinstall solaros-skel-base;
+  # util
+  mkNode = callPackage "${nixNodePackage}/nix/default.nix" { };
 
-  conf-tool = pkgs.callPackage ./conf-tool {
-    inherit mkNode;
-  };
-
-  solaros-skel-cinnamon = pkgs.callPackage ./solaros-skel-cinnamon {
-    inherit solaros-skel-base;
-  };
-  solaros-skel-mate = pkgs.callPackage ./solaros-skel-mate {
-    inherit solaros-skel-base;
-  };
-  solaros-skel-xfce = pkgs.callPackage ./solaros-skel-xfce {
-    inherit solaros-skel-base;
-  };
+  # a
+  # b
+  # c
+  conf-tool = callPackage ./conf-tool { };
+  # d
+  # e
+  # f
+  # g
+  # h
+  # i
+  # j
+  # k
+  # l
+  # m
+  makeIcon = callPackage ./make-icon {};
+  # n
+  nixinstall = lib.recurseIntoAttrs (callPackage "${installerSrc}/nix" { });
+  # o
+  # p
+  # q
+  # r
+  # s
+  solaros-skel-base = callPackage ./solaros-skel-base {};
+  # depend on skel-base
+  solaros-skel-cinnamon = callPackage ./solaros-skel-cinnamon { };
+  solaros-skel-mate = callPackage ./solaros-skel-mate { };
+  solaros-skel-xfce = callPackage ./solaros-skel-xfce { };
 
   solaros-installer = (nixinstall.nixinstall.override {
     # slideshowPackage = todo; # TODO: add slideshow support to elementary-installer
@@ -55,7 +63,7 @@ in
     '';
   });
 
-  solarosBundles = lib.makeScope pkgs.newScope (self:
+  solarosBundles = lib.makeScope newScope (self:
     let
       bundles = (import ../config/base/bundles.nix pkgs).config.solar.bundle;
     in
@@ -71,4 +79,11 @@ in
               description = if lib.hasAttrByPath ["description"] value then value.description else null;
             };
           }))]));
-}
+  # t
+  # u
+  # v
+  # w
+  # x
+  # y
+  # z
+})
--- pkgs/overlay.nix
+++ pkgs/overlay.nix
@@ -1,1 +1,20 @@
-self: super: (import ./all-packages.nix super)
+let
+  removeKeys = [ # if those keys aren't stripped we'll get infinite recursion as we're overwriting those base-keys
+    "callPackage"
+    "newScope"
+    "override"
+    "overrideDerivation"
+    "overrideScope"
+    "overrideScope'"
+    "packages"
+  ];
+
+  contains = thing: list:
+    (builtins.filter (otherThing: otherThing == thing) list) != [];
+
+  filter = lib: pkgs:
+    with lib;
+    filterAttrs (n: v: !(contains n removeKeys)) pkgs;
+in
+
+self: super: (filter super.lib (import ./all-packages.nix super))
